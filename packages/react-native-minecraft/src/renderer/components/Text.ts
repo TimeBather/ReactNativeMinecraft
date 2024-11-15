1b8f2673adac89e507c2f0405361bea1e76b3fee
TimeBather <open-source@timebather.cn>
2024-11-16 00:43:17 +0800

    fix: crash when there is children in Text node

--- packages/react-native-minecraft/src/renderer/components/Text.ts
+++ packages/react-native-minecraft/src/renderer/components/Text.ts
@@ -1,5 +1,9 @@
 import React, {ReactElement} from "react";
 
-export default function Canvas(props: any, container: any):ReactElement {
+export default function Text(props: any, container: any):ReactElement {
+    if(props.children){
+        console.warn("Props.children is not expected in this Node in current version: ", props.children);
+        delete props['children'];
+    }
     return React.createElement("text",props);
 }
