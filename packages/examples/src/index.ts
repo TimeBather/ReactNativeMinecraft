a83e94390121ba2520c7694944eb44888c09c11c
TimeBather <open-source@timebather.cn>
2024-11-26 15:23:43 +0800

    clean: remove examples

--- packages/examples/src/index.ts
+++
@@ -1,4 +0,0 @@
-import {AppRegistry} from "react-native-minecraft";
-import {} from "react-native"
-import App from "./AppTest";
-AppRegistry.registerGuiComponent("kasuga_lib:example",()=>App);
--- packages/examples/src/ping.ts
+++
@@ -1,2 +0,0 @@
-
-console.info("Metro Bundler works! 🎉")
