13ae4f2bef422d5b9bb1661bfa1e893087755482
TimeBather <open-source@timebather.cn>
2024-11-30 19:20:45 +0800

    fix(menu): type fix

--- packages/react-native-minecraft/src/debugger/hmr.ts
+++ packages/react-native-minecraft/src/debugger/hmr.ts
@@ -9,7 +9,7 @@ if(__DEV__){
     client.send(
         JSON.stringify({
             type: 'register-entrypoints',
-            entryPoints: [`ws://${globalThis['HMRServer'] ?? "127.0.0.1:8081"}/hot?bundleEntry=${globalThis['HMREntry'] ?? "client/index.bundle"}&platform=minecraft`],
+            entryPoints: [`ws://${globalThis['HMRServer' as keyof typeof globalThis] ?? "127.0.0.1:8081"}/hot?bundleEntry=${globalThis['HMREntry' as keyof typeof globalThis] ?? "client/index.bundle"}&platform=minecraft`],
         }),
     );
 }
